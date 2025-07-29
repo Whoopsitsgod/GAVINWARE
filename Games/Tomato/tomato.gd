6358612e1871f5d4ef78a010e1717fde6e319ae7
Whoopsitsgod <144404407+Whoopsitsgod@users.noreply.github.com>
2025-07-29 17:21:12 -0600

    finished tomato game

--- Games/Tomato/tomato.gd
+++ Games/Tomato/tomato.gd
@@ -1,13 +1,25 @@
-extends Control
+extends Node2D
+
+var won_game: bool = false
 
 signal finish_game(success: bool)
 
-
 func _ready() -> void:
-	$Jackson/Button0.pressed.connect(_on_pressed.bind("hello"))
+	$Jackson/Button.pressed.connect(_on_pressed)
+	$Jackson.play("default")
+	$Gavin.play("default")
 	
 func start_game(speed: int):
-	print("tomato has started")
+	await get_tree().create_timer(speed).timeout
+	finish_game.emit(finish_game)
 
-func _on_pressed(button_pressed: String):
-	print(button_pressed)
+func _on_pressed():
+	$Gavin.play("get_ready")
+	await get_tree().create_timer(0.5).timeout
+	$Gavin.play("hit")
+	$Jackson.play("struck")
+	var tween = create_tween()
+	tween.tween_property($Jackson, "position", Vector2(1200,240), 1.5)
+	tween.set_parallel()
+	tween.tween_property($Jackson, "rotation", 2, 1.5)
+	won_game = true
