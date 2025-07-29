39c185f435214855721c76d3f57617cd36a24aeb
Dallin Hoyt <dhhoyt2002@gmail.com>
2025-07-29 16:01:57 -0600

    Gavinware stuff 2

--- Games/FiberOpticCable/fiber_optic_cable.gd
+++ Games/FiberOpticCable/fiber_optic_cable.gd
@@ -4,5 +4,15 @@ signal finish_game(success: bool)
 
 var random_words = ["Gas", "Glass", "Bosom", "Wings", "Craigslist", "Apple"]
 # Called when the node enters the scene tree for the first time.
+
+func _ready() -> void:
+	$CenterContainer/GridContainer/Button0.pressed.connect(_on_pressed.bind(0))
+	$CenterContainer/GridContainer/Button1.pressed.connect(_on_pressed.bind(1))
+	$CenterContainer/GridContainer/Button2.pressed.connect(_on_pressed.bind(2))
+	$CenterContainer/GridContainer/Button3.pressed.connect(_on_pressed.bind(3))
+	
 func start_game(speed: int):
 	print("hi")
+
+func _on_pressed(button_num: int):
+	print(button_num)
