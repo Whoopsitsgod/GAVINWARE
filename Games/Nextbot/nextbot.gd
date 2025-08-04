971d3bef66e908e69366f688386df2d071472aa6
Whoopsitsgod <144404407+Whoopsitsgod@users.noreply.github.com>
2025-08-04 00:57:25 -0600

    added in correct sounds

--- Games/Nextbot/nextbot.gd
+++ Games/Nextbot/nextbot.gd
@@ -14,11 +14,13 @@ func _ready() -> void:
 	$NextbotSaul2/Button.pressed.connect(_on_pressed.bind($NextbotSaul2/Button))
 	$NextbotSaul3/Button.pressed.connect(_on_pressed.bind($NextbotSaul3/Button))
 	$NextbotSaul4/Button.pressed.connect(_on_pressed.bind($NextbotSaul4/Button))
+	$TweenedPlayer.play_tweened(load("res://H05 Crygor's Bathroom Jam 01.mp3"), 2)
 	
 func start_game(speed: int):
 	var time = transition_speed(speed)
 	$Timer.start_timer(time)
 	$Timer.timeout.connect(func(): game_finished.emit(won_game))
+	$Timer.timeout.connect(func(): $TweenedPlayer.stop_tweened(0.3))
 
 func _on_pressed(button: Node):
 	button.get_parent().visible = false
